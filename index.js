722bd091acab3145126e482b43bf4f8955ae757b
gergokutu <gergo.kutu@gmail.com>
2019-08-09 11:23:08 +0200

    Cors applied and corrected

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const Sse = require('json-sse')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 
 const messages = ['hello world']
 const sse = new Sse(messages)
