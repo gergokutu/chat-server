293d64fc6cab5e2f6622c2ef81c015eb24798492
gergokutu <gergo.kutu@gmail.com>
2019-08-09 14:14:02 +0200

    PostgreSQL db connected, GET and POST registered

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require('express')
 const cors = require('cors')
 const Sse = require('json-sse')
 const bodyParser = require('body-parser')
+const Sequilize = require('sequelize')
 
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/postgres'
+const db = new Sequilize(databaseUrl)
+
+db
+  .sync({ force: false })
+  .then(() => console.log('db synced'))
+
+const Message = db.define(
+  'message',
+  {
+    text: Sequilize.STRING
+  }
+)
 // starting message
-const messages = ['hello world']
-
+// we have db now » remove
+// const messages = ['hello world']
 // create string from JSON » serialize
-const data = JSON.stringify(messages)
-const stream = new Sse(data)
+// const data = JSON.stringify(messages)
+
+const stream = new Sse()
 
 const app = express()
 
@@ -21,23 +36,34 @@ app.use(jsonParser)
 // not calling just passing the sse.init function
 // keeps the request alive
 // with 1 request we can send many responses
-app.get('/stream', stream.init)
+app.get('/stream',
+  async (request, response) => {
+    const messages = await Message.findAll()
+    // serialization
+    const data = JSON.stringify(messages)
+    stream.updateInit(data)
+    stream.init(request, response)
+  }
+)
 
-app.post('/message', (request, response) => {
-  const { message } = request.body
-  messages.push(message)
+app.post('/message',
+  async (request, response) => {
+    const { message } = request.body
+    // using db instead og the initial array
+    // messages.push(message)
+    const entity = await Message.create({ text: message })
+    const messages = await Message.findAll()
+    // stringification
+    const data = JSON.stringify(messages)
 
-  // stringification
-  const data = JSON.stringify(messages)
+    stream.updateInit(data)
+    // broadcast (send everybody) all the messages with this
+    // so use messages instead of message
+    // stream.send(message)
+    stream.send(data)
 
-  stream.updateInit(data)
-  // broadcast (send everybody) all the messages with this
-  // so use messages instead of message
-  // stream.send(message)
-  stream.send(data)
-
-  response.send(messages)
-})
+    response.send(entity)
+  })
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`listeninportg on port: ${port}`))
