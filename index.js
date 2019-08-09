b963b278204cd5e1fef19ff850e856056737b015
gergokutu <gergo.kutu@gmail.com>
2019-08-09 10:25:50 +0200

    Server corrected » port for heroku

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
  const app = express()
  const port = process.env.PORT || 5000
 
- app.listen(5000, () => console.log(`listening on port: ${port}`))
+ app.listen(port, () => console.log(`listeninportg on port: ${port}`))
