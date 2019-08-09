59c5b3a5bdd5d78414012753e5f9fb28b4e08f4c
gergokutu <gergo.kutu@gmail.com>
2019-08-09 10:16:45 +0200

    Server running

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express')
  const app = express()
  const port = process.env.PORT || 5000
 
- app.listen(5000, () => console.log('listening...'))
+ app.listen(5000, () => console.log(`listening on port: ${port}`))
