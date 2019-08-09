5e3ea15aba96927e3f341daa04e7425d181b1109
gergokutu <gergo.kutu@gmail.com>
2019-08-09 10:40:39 +0200

    Get and post registered

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express')
- const app = express()
- const port = process.env.PORT || 5000
+const Sse = require('json-sse')
+const bodyParser = require('body-parser')
 
- app.listen(port, () => console.log(`listeninportg on port: ${port}`))
+const data = 'hello'
+const sse = new Sse(data)
+
+
+const app = express()
+
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
+
+// not calling just passing the sse.init function
+// keeps the request alive
+// with 1 request we can send many responses
+app.get('/stream', sse.init)
+
+app.post('/message', (request, response) => {
+  const {message} =request.body
+  sse.send(message)
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`listeninportg on port: ${port}`))
